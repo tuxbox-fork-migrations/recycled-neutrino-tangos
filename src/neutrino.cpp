tuxbox-fork-migrations/recycled-neutrino-tangos

commit 51a05f2aa66872f69298ee8a9e44b97e5af9d7a7

neutrino: generic-pc is now also using 1280x70 FB per default
Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/eb9eba8914dd968f2f9bcb7436721d4b5821c10d
Author: Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
Date: 2013-05-29 (Wed, 29 May 2013)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/neutrino.cpp
@@ -296,12 +296,6 @@ const lcd_setting_struct_t lcd_setting[SNeutrinoSettings::LCD_SETTING_COUNT] =
 #define DEFAULT_X_END_HD	1235 //1275
 #define DEFAULT_Y_END_HD	690  //715
 
-#if HAVE_GENERIC_HARDWARE
-#define DEFAULT_SCREEN_PRESET 0
-#else
-#define DEFAULT_SCREEN_PRESET 1
-#endif
-
 std::string ttx_font_file = "";
 
 int CNeutrinoApp::loadSetup(const char * fname)
@@ -687,12 +681,12 @@ int CNeutrinoApp::loadSetup(const char * fname)
 	g_settings.screen_StartY_lcd = configfile.getInt32( "screen_StartY_lcd", DEFAULT_Y_START_HD );
 	g_settings.screen_EndX_lcd = configfile.getInt32( "screen_EndX_lcd", DEFAULT_X_END_HD);
 	g_settings.screen_EndY_lcd = configfile.getInt32( "screen_EndY_lcd", DEFAULT_Y_END_HD);
-	g_settings.screen_preset = configfile.getInt32( "screen_preset", DEFAULT_SCREEN_PRESET);
+	g_settings.screen_preset = configfile.getInt32( "screen_preset", 1);
 
 #if HAVE_TRIPLEDRAGON
 	g_settings.screen_preset = 0; /* does not make sense to have two configurations for that... */
 #endif
-#if HAVE_SPARK_HARDWARE || HAVE_AZBOX_HARDWARE
+#if ! HAVE_COOLSTREAM
 	g_settings.screen_preset = 1; /* spark is now always using 1280x720 framebuffer */
 #endif
 	g_settings.screen_StartX = g_settings.screen_preset ? g_settings.screen_StartX_lcd : g_settings.screen_StartX_crt;

commit 05517d901c333e18e27b818f0d00df42c1cece11

fix screen mode selection logic error on TD
wrong screen mode default "LCD" was introduced with commit 51a05f2aa,
leading to HD framebuffer settings which don't make sense :-)


Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/2e5a63f70ccb1f83ab6fcaf7745bdd93c78e8dce
Author: Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
Date: 2013-08-03 (Sat, 03 Aug 2013)



------------------
This commit was generated by Migit

src/neutrino.cpp
@@ -687,8 +687,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 
 #if HAVE_TRIPLEDRAGON
 	g_settings.screen_preset = 0; /* does not make sense to have two configurations for that... */
-#endif
-#if ! HAVE_COOLSTREAM
+#elif ! HAVE_COOLSTREAM
 	g_settings.screen_preset = 1; /* spark is now always using 1280x720 framebuffer */
 #endif
 	g_settings.screen_StartX = g_settings.screen_preset ? g_settings.screen_StartX_lcd : g_settings.screen_StartX_crt;