tuxbox-fork-migrations/recycled-neutrino-tangos

commit 3cd9e10eabe904a55e72fd5ade8413531cdf2513

gui/personalize.cpp: fix CPinSetupNotifier: return false, menu repaint not needed
Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/404de5376b75c96bb3d79590b9cedc5cb8f8533e
Author: [CST] Focus <focus.cst@gmail.com>
Date: 2013-11-04 (Mon, 04 Nov 2013)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/gui/personalize.cpp
@@ -974,5 +974,6 @@ bool CPinSetupNotifier::changeNotify(const neutrino_locale_t, void *)
 {
 	toDisable->setActive(g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS]);
    
-   return g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS];
+//   return g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS];
+	return false;
 }

commit 60a47b4c110e23e19b987b9f9fbcc19aad03e1c9

gui/personalize.cpp: fix pin change active/enactive, broken after commit 3cd9e10eabe904a55e72fd5ade8413531cdf2513
Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/40e3106e5377127b946a9c4698e65fea3e36e2ba
Author: [CST] Focus <focus.cst@gmail.com>
Date: 2013-11-06 (Wed, 06 Nov 2013)



------------------
This commit was generated by Migit

src/gui/personalize.cpp
@@ -378,7 +378,7 @@ void CPersonalizeGui::ShowPinSetup(CMenuWidget* p_widget, CPINChangeWidget * &pi
  	pin_setup_notifier = new CPinSetupNotifier(fw_pin_setup);
  	p_widget->addItem(new CMenuOptionChooser(LOCALE_PERSONALIZE_PIN_IN_USE, &g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS], OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, pin_setup_notifier));
 	
-	fw_pin_setup->setActive(pin_setup_notifier->changeNotify());
+	pin_setup_notifier->changeNotify();
 	p_widget->addItem(fw_pin_setup);
 	
 	p_widget->addItem(new CMenuSeparator(CMenuSeparator::LINE | CMenuSeparator::STRING, LOCALE_PERSONALIZE_MENUCONFIGURATION));