f9b8f4c41e0c803aa657e67b75dd5d334d1535a0
TangoCash <eric@loxat.de>
2015-04-03 00:14:32 +0200

    fix last commit
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/8371e21c0c3b2ff2310fd51193f682192ac1bd95
    Author: TangoCash <eric@loxat.de>
    Date: 2015-04-03 (Fri, 03 Apr 2015)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -1683,7 +1683,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 	if (showButtonBar) {
 		if (!g_settings.info_top_gradiant)
 			frameBuffer->paintHLine(ChanInfoX + 10, BoxEndX - 10, CurrInfoY - height - 2, COL_INFOBAR_PLUS_3);
-		if (!g_settings.casystem_display)
+		if (g_settings.casystem_display < 2)
 			frameBuffer->paintHLine(ChanInfoX + 10, BoxEndX - 10, NextInfoY + 2, COL_INFOBAR_PLUS_3);
 	}
 
