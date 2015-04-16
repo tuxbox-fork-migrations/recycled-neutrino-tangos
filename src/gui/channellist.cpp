721f59936c4ee772e740ca2567154df7471e2929
TangoCash <eric@loxat.de>
2015-04-16 20:46:17 +0200

    fix osdleftovers when deactivating additional infos in channelist via channellistmenu
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/54be9028f123b3935553577700a94a988ee29fc7
    Author: TangoCash <eric@loxat.de>
    Date: 2015-04-16 (Thu, 16 Apr 2015)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -1670,7 +1670,7 @@ void CChannelList::clearItem2DetailsLine()
 
 void CChannelList::paintItem2DetailsLine (int pos)
 {
-	if (!g_settings.channellist_show_infobox)
+	if (!g_settings.channellist_show_infobox && pos != -1 )
 		return;
 
 	int xpos  = x - ConnectLineBox_Width;
