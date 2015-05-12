85ac174d5b3f509068b1eb1cc73a347bd0cc34ad
TangoCash <eric@loxat.de>
2015-05-12 22:17:57 +0200

    fix cherry-pick
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/0548d256e6843e0ca993a45aad2012d71b87e70a
    Author: TangoCash <eric@loxat.de>
    Date: 2015-05-12 (Tue, 12 May 2015)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/rcinput.h
+++ src/driver/rcinput.h
@@ -148,11 +148,7 @@ class CRCInput
 		int 		fd_pipe_low_priority[2];
 		int         	fd_gamerc;
 #if HAVE_SPARK_HARDWARE
-#if defined (BOXMODEL_SPARK) || defined (BOXMODEL_SPARK7162)
-#define NUMBER_OF_EVENT_DEVICES 2
-#else
 #define NUMBER_OF_EVENT_DEVICES 1 // this is currently valid for all supported platforms
-#endif
 #else
 #ifdef HAVE_DUCKBOX_HARDWARE
 #if defined (BOXMODEL_IPBOX9900) || defined (BOXMODEL_IPBOX99) || defined (BOXMODEL_IPBOX55)
