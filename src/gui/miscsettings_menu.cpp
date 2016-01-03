44aa8d5c9818aefa5bb5dd96057fe9bcac9d4cd5
TangoCash <eric@loxat.de>
2016-01-03 19:15:15 +0100

    (re-)enable kerneloptions
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/85fd2be1791ae0810b555eec99805a8d8422fb83
    Author: TangoCash <eric@loxat.de>
    Date: 2016-01-03 (Sun, 03 Jan 2016)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/miscsettings_menu.cpp
+++ src/gui/miscsettings_menu.cpp
@@ -43,6 +43,9 @@
 #include <gui/plugins.h>
 #include <gui/sleeptimer.h>
 #include <gui/zapit_setup.h>
+#if HAVE_SPARK_HARDWARE || HAVE_DUCKBOX_HARDWARE
+#include <gui/kerneloptions.h>
+#endif
 
 #include <gui/widget/icons.h>
 #include <gui/widget/stringinput.h>
@@ -320,6 +323,13 @@ int CMiscMenue::showMiscSettingsMenu()
 	mf->setHint("", LOCALE_MENU_HINT_MISC_CPUFREQ);
 	misc_menue.addItem(mf);
 #endif /*CPU_FREQ*/
+#if HAVE_SPARK_HARDWARE || HAVE_DUCKBOX_HARDWARE
+	// kerneloptions
+	CKernelOptions kernelOptions;
+	mf = new CMenuForwarder(LOCALE_KERNELOPTIONS_HEAD, true, NULL, &kernelOptions, NULL, CRCInput::RC_5);
+	mf->setHint("", LOCALE_MENU_HINT_MISC_KERNELOPTIONS);
+	misc_menue.addItem(mf);
+#endif
 
 	int res = misc_menue.exec(NULL, "");
 
