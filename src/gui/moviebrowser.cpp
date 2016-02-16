51b757618e88d5daace5fd51f9c2b04822815bb1
TangoCash <eric@loxat.de>
2016-02-16 22:34:19 +0100

    remove unneeded blits in moviebrowser
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/e572137ff9dd9a7e2b3d163d9a85036ee996849e
    Author: TangoCash <eric@loxat.de>
    Date: 2016-02-16 (Tue, 16 Feb 2016)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/moviebrowser.cpp
+++ src/gui/moviebrowser.cpp
@@ -1032,7 +1032,6 @@ int CMovieBrowser::exec(const char* path)
 	uint64_t timeoutEnd = CRCInput::calcTimeoutEnd(timeout);
 	while (loop)
 	{
-		framebuffer->blit();
 		g_RCInput->getMsgAbsoluteTimeout(&msg, &data, &timeoutEnd);
 
 		result = onButtonPress(msg);
@@ -1124,7 +1123,6 @@ int CMovieBrowser::exec(const char* path)
 			timeoutEnd = CRCInput::calcTimeoutEnd(timeout); // calcualate next timeout
 	}
 	hide();
-	framebuffer->blit();
 	//TRACE(" return %d\n",res);
 
 	m_prevBrowserSelection = m_currentBrowserSelection;
