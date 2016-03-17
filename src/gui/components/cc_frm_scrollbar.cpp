tuxbox-fork-migrations/recycled-neutrino-tangos

commit 431b6a3abe070bde1047eea10f80ab40121833d6

cc_frm_scrollbar.cpp icon dont need full path
Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/10c430d40e2b537882941c14f41f57724e4a87ba
Author: Jacek Jendrzej <overx300@gmail.com>
Date: 2016-02-16 (Tue, 16 Feb 2016)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/gui/components/cc_frm_scrollbar.cpp
@@ -81,8 +81,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_segments_obj = NULL;
 
 	string ftype = ".png";
-	sb_up_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
+	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;

commit 83a3f1203857e7a06afaa0922b2238828128b1a0

CComponentsScrollBar: fix missed navi arrows
Paths were broken
comes in with 431b6a3abe070bde1047eea10f80ab40121833d6


Origin commit data
------------------
Commit: https://github.com/TangoCash/neutrino-tangos/commit/c07f9994761406e495fb58778399b14720156678
Author: Thilo Graf <dbt@novatux.de>
Date: 2016-03-17 (Thu, 17 Mar 2016)



------------------
This commit was generated by Migit

src/gui/components/cc_frm_scrollbar.cpp
@@ -80,9 +80,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_up_obj 	= sb_down_obj = NULL;
 	sb_segments_obj = NULL;
 
-	string ftype = ".png";
-	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_TOP) ;
+	sb_down_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_DOWN);
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;