d091dcc9ee068c6c5e2d1c1d0e2e18378636674e
TangoCash <eric@loxat.de>
2017-02-13 17:16:42 +0100

    fix merge
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/d0821dd665c85b318d0c4cd718444e2967f2bf17
    Author: TangoCash <eric@loxat.de>
    Date: 2017-02-13 (Mon, 13 Feb 2017)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/eventlist.cpp
+++ src/gui/eventlist.cpp
@@ -53,6 +53,7 @@
 #include <driver/fade.h>
 #include <driver/record.h>
 #include <driver/fontrenderer.h>
+#include <gui/pictureviewer.h>
 
 #include <system/helpers.h>
 #include <zapit/client/zapittools.h>
