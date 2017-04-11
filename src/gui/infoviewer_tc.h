a7ae6c1e26b27d5503d9fe41cafcb5a3bb84326a
TangoCash <eric@loxat.de>
2017-04-11 20:45:51 +0200

    infoviewer_tc: format header
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/881ff1b0c2f5c07306ff26e69f48b520331b8ead
    Author: TangoCash <eric@loxat.de>
    Date: 2017-04-11 (Tue, 11 Apr 2017)
    
    Origin message was:
    ------------------
    - infoviewer_tc: format header
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/infoviewer_tc.h
+++ src/gui/infoviewer_tc.h
@@ -49,294 +49,318 @@ class CFrameBuffer;
 class COSDFader;
 class CInfoViewer
 {
- public:
- 	enum
-	{
-		// The order of icons from left to right. Here nothing changes!
-		BUTTON_RED	= 0,
-		BUTTON_GREEN	= 1,
-		BUTTON_YELLOW	= 2,
-		BUTTON_BLUE	= 3,
-		BUTTON_MAX	= 4
-	};
- private:
+public:
+    enum
+    {
+        // The order of icons from left to right. Here nothing changes!
+        BUTTON_RED	= 0,
+        BUTTON_GREEN	= 1,
+        BUTTON_YELLOW	= 2,
+        BUTTON_BLUE	= 3,
+        BUTTON_MAX	= 4
+    };
+private:
 
-	CFrameBuffer * frameBuffer;
-	CComponentsFrmClock *clock;
-	CComponentsShapeSquare *header , *numbox, *body, *rec;
-	CComponentsTextTransp *txt_cur_start, *txt_cur_event, *txt_cur_event_rest, *txt_next_start, *txt_next_event, *txt_next_in;
+    CFrameBuffer *frameBuffer;
+    CComponentsFrmClock *clock;
+    CComponentsShapeSquare *header, *body, *rec, *foot, *ca_bar;
+    CComponentsTextTransp *txt_cur_start, *txt_cur_event, *txt_cur_event_rest, *txt_next_start, *txt_next_event, *txt_next_in;
 
-	bool           gotTime;
-	bool           recordModeActive;
+    bool           gotTime;
+    bool           recordModeActive;
 
 #ifndef SKIP_CA_STATUS
-	bool           CA_Status;
+    bool           CA_Status;
 #endif
-	
-	int            InfoHeightY;
-	bool	       fileplay;
 
-	int            ButtonWidth;
-	int            spacer;
+    int            InfoHeightY;
+    bool	       fileplay;
 
-        // dimensions of radiotext window
-        int             rt_dx;
-        int             rt_dy;
-        int             rt_x;
-        int             rt_y;
-        int             rt_h;
-        int             rt_w;
-	
-	std::string ChannelName;
+    int            ButtonWidth;
+    int            spacer;
 
-	int            ChanNameX;
-	int            ChanNumWidth;
-	int            ChanWidth;
-	int            numbox_offset;
+    // dimensions of radiotext window
+    int             rt_dx;
+    int             rt_dy;
+    int             rt_x;
+    int             rt_y;
+    int             rt_h;
+    int             rt_w;
 
-	int            ana_clock_size;
-	char		   strChanNum[10];
-	void           PaintChanNumber();
+    std::string ChannelName;
 
-	CSectionsdClient::CurrentNextInfo info_CurrentNext;
-	CSectionsdClient::CurrentNextInfo oldinfo;
-        t_channel_id   current_channel_id;
-        t_channel_id   current_epg_id;
+    int            ChanNameX;
+    int            ChanNumWidth;
+    int            ChanWidth;
+    int            numbox_offset;
 
-	//uint32_t           fadeTimer;
-	COSDFader	fader;
+    int            ana_clock_size;
+    char		   strChanNum[10];
+    void           PaintChanNumber();
 
-	int time_height;
-	int info_time_width;
-	int header_height;
-	bool newfreq ;
-	static const short bar_width = 72;
-	static event_id_t last_curr_id, last_next_id;
-	uint64_t timeoutEnd;
-	void setInfobarTimeout(int timeout_ext = 0);
+    CSectionsdClient::CurrentNextInfo info_CurrentNext;
+    CSectionsdClient::CurrentNextInfo oldinfo;
+    t_channel_id   current_channel_id;
+    t_channel_id   current_epg_id;
 
-	CChannelEventList               evtlist;
-	CChannelEventList::iterator     eli;
+    //uint32_t           fadeTimer;
+    COSDFader	fader;
 
-	int lastsnr, lastsig, lasttime;
-	CProgressBar *timescale;
-	CSignalBox *sigbox;
+    int time_height;
+    int info_time_width;
+    int header_height;
+    bool newfreq ;
+    static const short bar_width = 72;
+    static event_id_t last_curr_id, last_next_id;
+    uint64_t timeoutEnd;
+    void setInfobarTimeout(int timeout_ext = 0);
 
-	bool casysChange;
-	bool channellogoChange;
-	uint32_t lcdUpdateTimer;
-	int	 zap_mode;
-	std::string _livestreamInfo1;
-	std::string _livestreamInfo2;
+    CChannelEventList               evtlist;
+    CChannelEventList::iterator     eli;
 
-	void paintBackground(int col_Numbox);
-	void paintHead();
-	void paintBody();
-	void show_Data( bool calledFromEvent = false );
-	void display_Info(const char *current, const char *next,
-			  bool starttimes = true, const int pb_pos = -1,
-			  const char *runningStart = NULL, const char *runningRest = NULL,
-			  const char *nextStart = NULL, const char *nextDuration = NULL,
-			  bool update_current = true, bool update_next = true);
-	void initClock();
-	void showRecordIcon(const bool show);
-	void showIcon_Tuner() const;
+    int lastsnr, lastsig, lasttime;
+    CProgressBar *timescale;
+    CSignalBox *sigbox;
 
-	void showFailure();
-	void showMotorMoving(int duration);
-   	void showLcdPercentOver();
-	int showChannelLogo(const t_channel_id logo_channel_id, const int channel_number_width);
-	void showRadiotext();
-	void killRadiotext();
+    bool casysChange;
+    bool channellogoChange;
+    uint32_t lcdUpdateTimer;
+    int	 zap_mode;
+    std::string _livestreamInfo1;
+    std::string _livestreamInfo2;
 
-	//small infobox, shows a small textbox with a short message text,
-	//text must be located in a file named /tmp/infobar.txt
-	CComponentsInfoBox *infobar_txt;
-	void showInfoFile();
-	void killInfobarText();
+    void paintBackground(int col_Numbox);
+    void paintHead();
+    void paintBody();
+    void show_Data( bool calledFromEvent = false );
+    void display_Info(const char *current, const char *next,
+                      bool starttimes = true, const int pb_pos = -1,
+                      const char *runningStart = NULL, const char *runningRest = NULL,
+                      const char *nextStart = NULL, const char *nextDuration = NULL,
+                      bool update_current = true, bool update_next = true);
+    void initClock();
+    void showRecordIcon(const bool show);
+    void showIcon_Tuner() const;
 
-	//void loop(int fadeValue, bool show_dot ,bool fadeIn);
-	void loop(bool show_dot);
-	std::string eventname;
-	void show_current_next(bool new_chan, int  epgpos);
-	void reset_allScala();
-	void check_channellogo_ca_SettingsChange();
-	void sendNoEpg(const t_channel_id channel_id);
-	bool showLivestreamInfo();
+    void showFailure();
+    void showMotorMoving(int duration);
+    void showLcdPercentOver();
+    int showChannelLogo(const t_channel_id logo_channel_id, const int channel_number_width);
+    void showRadiotext();
+    void killRadiotext();
 
-	CComponentsWindowMax *ecmInfoBox;
+    //small infobox, shows a small textbox with a short message text,
+    //text must be located in a file named /tmp/infobar.txt
+    CComponentsInfoBox *infobar_txt;
+    void showInfoFile();
+    void killInfobarText();
 
- 		enum
-		{
-			// The order of icons from right to left. Here nothing changes!
-			ICON_SUBT	= 0,
-			ICON_VTXT	= 1,
-			ICON_RT		= 2,
-			ICON_DD		= 3,
-			ICON_16_9	= 4,
-			ICON_RES	= 5,
-			ICON_CA		= 6,
-			ICON_TUNER	= 7,
-			ICON_UPDATE	= 8,
-			ICON_LOGO	= 9,
-			ICON_MAX	= 10
-		};
+    //void loop(int fadeValue, bool show_dot ,bool fadeIn);
+    void loop(bool show_dot);
+    std::string eventname;
+    void show_current_next(bool new_chan, int  epgpos);
+    void reset_allScala();
+    void check_channellogo_ca_SettingsChange();
+    void sendNoEpg(const t_channel_id channel_id);
+    bool showLivestreamInfo();
 
-		typedef struct
-		{
-			bool paint;
-			int w;
-			int x;
-			int cx;
-			int h;
-			std::string icon;
-			std::string text;
-			bool active;
-		} bbButtonInfoStruct;
+    CComponentsWindowMax *ecmInfoBox;
 
-		enum CAM_DECODE_NUM {UNKNOWN, NA, FTA, LOCAL, CARD, REMOTE};
-		void 		paint_cam_icons();
-		void*		Thread_paint_cam_icons(void);
-		unsigned short int decode;
-		int		DecEndx;
-		int 		parse_ecmInfo(const char * file);
-		int		check_ecmInfo();
-		bool	camCI;
-		bool	useCI;
+    enum
+    {
+        // The order of icons from right to left. Here nothing changes!
+        ICON_SUBT	= 0,
+        ICON_VTXT	= 1,
+        ICON_RT		= 2,
+        ICON_DD		= 3,
+        ICON_16_9	= 4,
+        ICON_RES	= 5,
+        ICON_CA		= 6,
+        ICON_TUNER	= 7,
+        ICON_UPDATE	= 8,
+        ICON_LOGO	= 9,
+        ICON_MAX	= 10
+    };
 
-		typedef struct
-		{
-			int x;
-			int h;
-		} bbIconInfoStruct;
+    typedef struct
+    {
+        bool paint;
+        int w;
+        int x;
+        int cx;
+        int h;
+        std::string icon;
+        std::string text;
+        bool active;
+    } bbButtonInfoStruct;
 
-		bbIconInfoStruct bbIconInfo[ICON_MAX];
-		bbButtonInfoStruct bbButtonInfo[BUTTON_MAX];
-		std::string tmp_bbButtonInfoText[BUTTON_MAX];
-		int bbIconMinX, bbButtonMaxX, bbIconMaxH, bbButtonMaxH;
+    enum CAM_DECODE_NUM {UNKNOWN, NA, FTA, LOCAL, CARD, REMOTE};
+    void 		paint_cam_icons();
+    void*		Thread_paint_cam_icons(void);
+    unsigned short int decode;
+    int		DecEndx;
+    int 		parse_ecmInfo(const char * file);
+    int		check_ecmInfo();
+    bool	camCI;
+    bool	useCI;
 
-		int BBarY, BBarFontY;
-		int hddwidth;
-		//int lasthdd, lastsys;
-		bool fta;
-		int minX;
+    typedef struct
+    {
+        int x;
+        int h;
+    } bbIconInfoStruct;
 
-		bool scrambledErr, scrambledErrSave;
-		bool scrambledNoSig, scrambledNoSigSave;
-		pthread_t scrambledT;
+    bbIconInfoStruct bbIconInfo[ICON_MAX];
+    bbButtonInfoStruct bbButtonInfo[BUTTON_MAX];
+    std::string tmp_bbButtonInfoText[BUTTON_MAX];
+    int bbIconMinX, bbButtonMaxX, bbIconMaxH, bbButtonMaxH;
 
-		CProgressBar *hddscale, *sysscale;
-		CComponentsShapeSquare *foot, *ca_bar;
-		void paintFoot(int w = 0);
-		void showBBIcons(const int modus, const std::string & icon);
-		void getBBIconInfo(void);
-		bool checkBBIcon(const char * const icon, int *w, int *h);
+    int BBarY, BBarFontY;
+    int hddwidth;
+    //int lasthdd, lastsys;
+    bool fta;
+    int minX;
 
-		void paint_ca_icons(int, const char*, int&);
-		void paint_ca_bar();
-		void showOne_CAIcon();
+    bool scrambledErr, scrambledErrSave;
+    bool scrambledNoSig, scrambledNoSigSave;
+    pthread_t scrambledT;
 
-		static void* scrambledThread(void *arg);
-		void scrambledCheck(bool force=false);
+    CProgressBar *hddscale, *sysscale;
+    void paintFoot(int w = 0);
+    void showBBIcons(const int modus, const std::string & icon);
+    void getBBIconInfo(void);
+    bool checkBBIcon(const char * const icon, int *w, int *h);
 
-		void showBarSys(int percent = 0);
-		void showBarHdd(int percent = 0);
+    void paint_ca_icons(int, const char*, int&);
+    void paint_ca_bar();
+    void showOne_CAIcon();
+
+    static void* scrambledThread(void *arg);
+    void scrambledCheck(bool force=false);
+
+    void showBarSys(int percent = 0);
+    void showBarHdd(int percent = 0);
 
 public:
-	bool     chanready;
-	bool	 is_visible;
+    bool     chanready;
+    bool	 is_visible;
 
-	char     aspectRatio;
-	uint32_t sec_timer_id;
+    char     aspectRatio;
+    uint32_t sec_timer_id;
 
-	int      BoxStartX;
-	int      BoxStartY;
-	int      ChanHeight;
-	int      BoxEndX;
-	int      BoxEndY;
-	int      ChanInfoX;
-	bool     showButtonBar;
-	bool     isVolscale;
-	int      ChanNameY;
-	int      time_width;
+    int      BoxStartX;
+    int      BoxStartY;
+    int      ChanHeight;
+    int      BoxEndX;
+    int      BoxEndY;
+    int      ChanInfoX;
+    bool     showButtonBar;
+    bool     isVolscale;
+    int      ChanNameY;
+    int      time_width;
 
-	std::string	md5_ecmInfo;
-	bool ecminfo_toggle;
-	void ecmInfoBox_hide();
-	void ecmInfoBox_show(const char * txt, int w, int h, Font * font);
+    std::string	md5_ecmInfo;
+    bool ecminfo_toggle;
+    void ecmInfoBox_hide();
+    void ecmInfoBox_show(const char * txt, int w, int h, Font * font);
 
-	CInfoViewer();
-	~CInfoViewer();
+    CInfoViewer();
+    ~CInfoViewer();
 
-	void	showMovieTitle(const int playState, const t_channel_id &channel_id, const std::string &title,
-				const std::string &g_file_epg, const std::string &g_file_epg1,
-				const int duration, const int curr_pos, const int repeat_mode, const int _zap_mode = IV_MODE_DEFAULT);
+    void	showMovieTitle(const int playState, const t_channel_id &channel_id, const std::string &title,
+                           const std::string &g_file_epg, const std::string &g_file_epg1,
+                           const int duration, const int curr_pos, const int repeat_mode, const int _zap_mode = IV_MODE_DEFAULT);
 
-	void	start();
-	void	showEpgInfo();
-	void	showTitle(CZapitChannel * channel, const bool calledFromNumZap = false, int epgpos = 0);
-	void	showTitle(t_channel_id channel_id, const bool calledFromNumZap = false, int epgpos = 0);
-	void lookAheadEPG(const int ChanNum, const std::string & Channel, const t_channel_id new_channel_id = 0, const bool calledFromNumZap = false); //alpha: fix for nvod subchannel update
-	void	killTitle();
-	void	getEPG(const t_channel_id for_channel_id, CSectionsdClient::CurrentNextInfo &info);
-	
-	void	showSubchan();
-	//void	Set_CA_Status(int Status);
-	
-	int     handleMsg(const neutrino_msg_t msg, neutrino_msg_data_t data);
+    void	start();
+    void	showEpgInfo();
+    void	showTitle(CZapitChannel * channel, const bool calledFromNumZap = false, int epgpos = 0);
+    void	showTitle(t_channel_id channel_id, const bool calledFromNumZap = false, int epgpos = 0);
+    void lookAheadEPG(const int ChanNum, const std::string & Channel, const t_channel_id new_channel_id = 0, const bool calledFromNumZap = false); //alpha: fix for nvod subchannel update
+    void	killTitle();
+    void	getEPG(const t_channel_id for_channel_id, CSectionsdClient::CurrentNextInfo &info);
 
-	enum{
-		IV_MODE_DEFAULT		= 0,
-		IV_MODE_VIRTUAL_ZAP 	= 1,
-		IV_MODE_NUMBER_ZAP	= 2
-	};/*iv_switch_mode_t*/
-	/**sets mode for infoviewer.
-	* @param[in]  mode
-	* 	@li IV_MODE_DEFAULT
-	* 	@li IV_MODE_VIRTUAL_ZAP 	means the virtual zap mode, user is typing keys for virtual channel switch
-	* 	@li IV_MODE_NUMBER_ZAP 		means number mode, user is typing number keys into screen
-	* @return
-	*	void
-	* @see
-	* 	resetSwitchMode()
-	* 	getSwitchMode()
-	*/
-	void	setSwitchMode(const int& mode) {zap_mode = mode;}
-	int	getSwitchMode() {return zap_mode;}
-	void    resetSwitchMode() {setSwitchMode(IV_MODE_DEFAULT);}
+    void	showSubchan();
+    //void	Set_CA_Status(int Status);
 
-	void    changePB();
-	void 	ResetPB();
-	void    showSNR();
-	void    Init(void);
-	bool    SDT_freq_update;
-	void	setUpdateTimer(uint64_t interval);
-	uint32_t getUpdateTimer(void) { return lcdUpdateTimer; }
-	inline t_channel_id get_current_channel_id(void) { return current_channel_id; }
-	void 	ResetModules(bool kill = false);
-	void	KillModules() {ResetModules(true); };
+    int     handleMsg(const neutrino_msg_t msg, neutrino_msg_data_t data);
 
-	int bottom_bar_offset, InfoHeightY_Info, showBBIcons_width;
+    enum
+    {
+        IV_MODE_DEFAULT		= 0,
+        IV_MODE_VIRTUAL_ZAP 	= 1,
+        IV_MODE_NUMBER_ZAP	= 2
+    };/*iv_switch_mode_t*/
+    /**sets mode for infoviewer.
+    * @param[in]  mode
+    * 	@li IV_MODE_DEFAULT
+    * 	@li IV_MODE_VIRTUAL_ZAP 	means the virtual zap mode, user is typing keys for virtual channel switch
+    * 	@li IV_MODE_NUMBER_ZAP 		means number mode, user is typing number keys into screen
+    * @return
+    *	void
+    * @see
+    * 	resetSwitchMode()
+    * 	getSwitchMode()
+    */
+    void	setSwitchMode(const int& mode)
+    {
+        zap_mode = mode;
+    }
+    int	getSwitchMode()
+    {
+        return zap_mode;
+    }
+    void    resetSwitchMode()
+    {
+        setSwitchMode(IV_MODE_DEFAULT);
+    }
 
-	void showSysfsHdd(void);
-	void showIcon_CA_Status(int);
-	void showIcon_16_9();
-	void showIcon_RadioText(bool rt_available);
-	void showIcon_VTXT();
-	void showIcon_SubT();
-	void showIcon_Resolution();
-	void showIcon_Tuner(void);
-	void showIcon_Update(bool);
-	void showIcon_Logo();
-	void show_clock(int posx,int posy,int dia);
-	void ShowRecDirScale();
-	void showIcon_DD(void);
-	void showBBButtons(bool paintFooter = false);
-	void paintshowButtonBar();
-	void getBBButtonInfo(void);
-	void initBBOffset(void);
-	// modules
-	CComponentsShapeSquare* getFooter(void){return foot;}
-	CComponentsShapeSquare* getCABar(void){return ca_bar;}
+    void    changePB();
+    void 	ResetPB();
+    void    showSNR();
+    void    Init(void);
+    bool    SDT_freq_update;
+    void	setUpdateTimer(uint64_t interval);
+    uint32_t getUpdateTimer(void)
+    {
+        return lcdUpdateTimer;
+    }
+    inline t_channel_id get_current_channel_id(void)
+    {
+        return current_channel_id;
+    }
+    void 	ResetModules(bool kill = false);
+    void	KillModules()
+    {
+        ResetModules(true);
+    };
+
+    int bottom_bar_offset, InfoHeightY_Info, showBBIcons_width;
+
+    void showSysfsHdd(void);
+    void showIcon_CA_Status(int);
+    void showIcon_16_9();
+    void showIcon_RadioText(bool rt_available);
+    void showIcon_VTXT();
+    void showIcon_SubT();
+    void showIcon_Resolution();
+    void showIcon_Tuner(void);
+    void showIcon_Update(bool);
+    void showIcon_Logo();
+    void show_clock(int posx,int posy,int dia);
+    void ShowRecDirScale();
+    void showIcon_DD(void);
+    void showBBButtons(bool paintFooter = false);
+    void paintshowButtonBar();
+    void getBBButtonInfo(void);
+    void initBBOffset(void);
+    // modules
+    CComponentsShapeSquare* getFooter(void)
+    {
+        return foot;
+    }
+    CComponentsShapeSquare* getCABar(void)
+    {
+        return ca_bar;
+    }
 };
 #endif
