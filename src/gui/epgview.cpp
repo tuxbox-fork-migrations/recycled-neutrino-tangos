a314708ec6e1a682b0eea12d0731ed52a44541b0
TangoCash <eric@loxat.de>
2017-05-21 19:09:29 +0200

    fix after merge
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/21d79126ca0605bbd73370224e4e68fbbffa0a49
    Author: TangoCash <eric@loxat.de>
    Date: 2017-05-21 (Sun, 21 May 2017)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/epgview.cpp
+++ src/gui/epgview.cpp
@@ -850,7 +850,7 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 		header->enableColBodyGradient(g_settings.theme.menu_Head_gradient, COL_MENUCONTENT_PLUS_0, g_settings.theme.menu_Head_gradient_direction);
 		header->enableClock(true, "%H:%M", "%H.%M", true);
 		header->getClockObject()->setBlit(false);
-	}else
+	} else {
 		header->setDimensionsAll(sx, sy, ox, toph);
 		header->getClockObject()->setBlit(false);
 	}
