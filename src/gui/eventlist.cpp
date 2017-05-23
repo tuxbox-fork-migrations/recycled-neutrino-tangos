29dc3bb8c152bc9d9ce25fc51e0fe8acb7543f43
TangoCash <eric@loxat.de>
2017-05-23 23:23:49 +0200

    font and logo fixes (ported from NI)
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/17715fc3be1d983ffd4d183a44e2f8c22d5bf7ea
    Author: TangoCash <eric@loxat.de>
    Date: 2017-05-23 (Tue, 23 May 2017)
    
    Origin message was:
    ------------------
    -font and logo fixes (ported from NI)
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/eventlist.cpp
+++ src/gui/eventlist.cpp
@@ -323,7 +323,7 @@ int CEventList::exec(const t_channel_id channel_id, const std::string& channelna
 	iheight = ::paintButtons(0, 0, 0, 1, tmp_button, 0, 0, "", false, COL_MENUFOOT_TEXT, NULL, 0, false);
 
 	// Calculate theight
-	theight = g_Font[SNeutrinoSettings::FONT_TYPE_EVENTLIST_TITLE]->getHeight();
+	theight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
 	const int pic_h = 39;
 	theight = std::max(theight, pic_h);
 
@@ -891,7 +891,8 @@ void CEventList::paintHead(t_channel_id _channel_id, std::string _channelname, s
 
 	if (header->isPainted())
 		header->getChannelLogoObject()->hide();
-	header->setChannelLogo(_channel_id,_channelname);
+	if (g_settings.channellist_show_channellogo)
+		header->setChannelLogo(_channel_id,_channelname);
 	header->setCaption(_channelname);
 
 	header->paint(CC_SAVE_SCREEN_NO);
