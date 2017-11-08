83188483c9610d8c7fdc31e198021b5c5cdd708a
TangoCash <eric@loxat.de>
2017-11-08 14:30:15 +0100

    unify and rework screenshots, always use external grab
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/c96c4e1a2f565ea2cae1b702e02e0816613c8914
    Author: TangoCash <eric@loxat.de>
    Date: 2017-11-08 (Wed, 08 Nov 2017)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/screenshot.h
+++ src/driver/screenshot.h
@@ -35,58 +35,28 @@ class CScreenShot
 		typedef enum {
 			FORMAT_PNG,
 			FORMAT_JPG,
-			FORMAT_BMP
 		} screenshot_format_t;
 
 	private:
 		screenshot_format_t format;
 		std::string filename;
-		unsigned char * pixel_data;
 		int xres;
 		int yres;
 		bool extra_osd;
 		bool get_osd;
 		bool get_video;
-		bool scale_to_video;
-#if !HAVE_SH4_HARDWARE
-		FILE *fd;
-		pthread_t  scs_thread;
-		pthread_mutex_t thread_mutex;
-		pthread_mutex_t getData_mutex;
-
-		bool GetData();
-		bool OpenFile();
-		bool SaveFile();
-
-		bool SavePng();
-		bool SaveJpg();
-		bool SaveBmp();
-
-		bool startThread();
-		static void* initThread(void *arg);
-		void runThread();
-		static void cleanupThread(void *arg);
-#endif
-
-#ifdef BOXMODEL_CS_HD2
-		bool mergeOsdScreen(uint32_t dx, uint32_t dy, fb_pixel_t* osdData);
-#endif
+		bool scale_to_osd;
 
 	public:
-		CScreenShot(const std::string fname = "", screenshot_format_t fmt = CScreenShot::FORMAT_JPG);
+		CScreenShot(const std::string fname = "", screenshot_format_t fmt = CScreenShot::FORMAT_PNG);
 		~CScreenShot();
 
 		void MakeFileName(const t_channel_id channel_id);
 		void SetSize(int w, int h) { xres = w; yres = h; }
 		void EnableVideo(bool enable) { get_video = enable; }
 		void EnableOSD(bool enable) { get_osd = enable; }
-		void ScaleToVideo(bool enable) { scale_to_video = enable; }
-#if HAVE_COOL_HARDWARE
-		bool Start();
-#else
+		void ScaleToOSD(bool enable) { scale_to_osd = enable; }
 		bool Start(const std::string custom_cmd = "");
-#endif
-		bool StartSync();
 };
 
 #endif
