2bf55431c00a009b5aff4a2da0678b7fdd5f556f
TangoCash <eric@loxat.de>
2017-11-08 17:50:07 +0100

    scrennshot: remove unused
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/c27012985747f79d9da33de86c790b5de6affa1d
    Author: TangoCash <eric@loxat.de>
    Date: 2017-11-08 (Wed, 08 Nov 2017)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/nhttpd/yhttpd_core/helper.cpp
+++ src/nhttpd/yhttpd_core/helper.cpp
@@ -399,12 +399,6 @@ std::string json_convert_string(std::string s) {
 #endif // 0
 
 std::string yExecuteScript(std::string cmd) {
-#if HAVE_SH4_HARDWARE
-	const char *fbshot = "Y_Tools fbshot fb /";
-	int len = strlen(fbshot);
-	if (!strncmp(cmd.c_str(), fbshot, len))
-		return CFrameBuffer::getInstance()->OSDShot(cmd.substr(len - 1)) ? "" : "error";
-#endif
 
 	std::string script, para, result;
 	bool found = false;
