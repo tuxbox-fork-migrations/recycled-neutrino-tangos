4963c4674400601a299fbb5a41a616e5212edb32
TangoCash <eric@loxat.de>
2018-01-03 21:02:23 +0100

    fix compile
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/ad24b438d16ec84f66a8ee5f1d62be7d9f1a7b72
    Author: TangoCash <eric@loxat.de>
    Date: 2018-01-03 (Wed, 03 Jan 2018)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -3079,9 +3079,11 @@ void CNeutrinoApp::RealRun()
 				if (g_settings.recording_type != CNeutrinoApp::RECORDING_OFF)
 					CRecordManager::getInstance()->exec(NULL, "Record");
 			}
+#if 0
 			else if ((mode == NeutrinoModes::mode_webtv) && msg == (neutrino_msg_t) g_settings.mpkey_subtitle) {
 				CMoviePlayerGui::getInstance(true).selectSubtitle();
 			}
+#endif
 			/* after sensitive key bind, check user menu */
 			else if (usermenu.showUserMenu(msg)) {
 			}
@@ -3488,10 +3490,12 @@ int CNeutrinoApp::handleMsg(const neutrino_msg_t _msg, neutrino_msg_data_t data)
 		}
 		return messages_return::handled;
 	}
+#if 0
 	if (mode == NeutrinoModes::mode_webtv && msg == NeutrinoMessages::EVT_SUBT_MESSAGE) {
 		CMoviePlayerGui::getInstance(true).showSubtitle(data);
 		return messages_return::handled;
 	}
+#endif
 	if (msg == NeutrinoMessages::EVT_AUTO_SET_VIDEOSYSTEM) {
 		printf(">>>>>[CNeutrinoApp::%s:%d] Receive EVT_AUTO_SET_VIDEOSYSTEM message\n", __func__, __LINE__);
 		COsdHelpers *coh = COsdHelpers::getInstance();
@@ -5369,8 +5373,10 @@ void CNeutrinoApp::StopSubtitles(bool enable_glcd_mirroring)
 	if (enable_glcd_mirroring)
 		nGLCD::MirrorOSD(g_settings.glcd_mirror_osd);
 #endif
+#if 0
 	if (mode == NeutrinoModes::mode_webtv)
 		CMoviePlayerGui::getInstance(true).clearSubtitle(true);
+#endif
 }
 
 void CNeutrinoApp::StartSubtitles(bool show)
@@ -5387,8 +5393,10 @@ void CNeutrinoApp::StartSubtitles(bool show)
 		return;
 	dvbsub_start(0);
 	tuxtx_pause_subtitle(false);
+#if 0
 	if (mode == NeutrinoModes::mode_webtv)
 		CMoviePlayerGui::getInstance(true).clearSubtitle(false);
+#endif
 }
 
 void CNeutrinoApp::SelectSubtitles()
