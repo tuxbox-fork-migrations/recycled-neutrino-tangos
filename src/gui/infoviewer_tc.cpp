9855c0d59617e2b94ec72ef89b6bf66a20bd1531
TangoCash <eric@loxat.de>
2018-12-24 13:50:40 +0100

    show event logos only in infoviewer
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/da36e00067c17ad1463d9a0240a9f4a705aaa7c4
    Author: TangoCash <eric@loxat.de>
    Date: 2018-12-24 (Mon, 24 Dec 2018)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/infoviewer_tc.cpp
+++ src/gui/infoviewer_tc.cpp
@@ -398,6 +398,7 @@ void CInfoViewer::paintHead(t_channel_id channel_id,std::string channel_name)
 		time_width = header->getClockObject()->getWidth();
 	}
 
+	header->getChannelLogoObject()->enableEventLogo(true);
 	header->setChannelLogo(channel_id,channel_name,CCHeaderTypes::CC_LOGO_LEFT);
 	header->setCaptionMaxWidth(header->getWidth() - time_width - 3*OFFSET_INNER_MID - showBBIcons_width);
 
