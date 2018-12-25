c52f1d515580ab71c37aa799ef1131d648f123c3
TangoCash <eric@loxat.de>
2018-12-25 11:58:58 +0100

    fix after merge
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/2a89a57e9d8e1559fd025003012ebef2bce89fe9
    Author: TangoCash <eric@loxat.de>
    Date: 2018-12-25 (Tue, 25 Dec 2018)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/infoviewer_tc.cpp
+++ src/gui/infoviewer_tc.cpp
@@ -81,7 +81,7 @@
 #include <zapit/zapit.h>
 #include <zapit/capmt.h>
 #include <eitd/sectionsd.h>
-#include <video.h>
+#include <hardware/video.h>
 
 extern CRemoteControl *g_RemoteControl;	/* neutrino.cpp */
 extern CBouquetList * bouquetList;       /* neutrino.cpp */
