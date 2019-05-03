399e2677efa5463de22b62b27a10c89be542b9cf
max_10 <max_10@gmx.de>
2019-05-03 19:27:35 +0200

    activate unicable pin, thx DboxOldie
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/76c4ee3b101f610649f459b512ab7324e39e1711
    Author: max_10 <max_10@gmx.de>
    Date: 2019-05-03 (Fri, 03 May 2019)
    
    Origin message was:
    ------------------
    - activate unicable pin, thx DboxOldie
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2660,6 +2660,8 @@ const char * locale_real_names[] =
 	"tunersetup.power",
 	"tunersetup.terr",
 	"unicable.lnb",
+	"unicable.pin",
+	"unicable.pin_hint",
 	"unicable.qrg",
 	"unicable.scr",
 	"unit.decimal",
