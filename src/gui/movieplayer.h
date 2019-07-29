8195fa982cf95d172f621a22941b671ee0924d50
TangoCash <eric@loxat.de>
2019-07-29 19:37:04 +0200

    fix chapters
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/4c6c66fcddcefa2c8f3b5cc075e15e84f1b0ccd9
    Author: TangoCash <eric@loxat.de>
    Date: 2019-07-29 (Mon, 29 Jul 2019)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/movieplayer.h
+++ src/gui/movieplayer.h
@@ -248,6 +248,8 @@ class CMoviePlayerGui : public CMenuTarget
 #if 0
 	void selectSubtitle();
 	bool convertSubtitle(std::string &text);
+#endif
+#if HAVE_ARM_HARDWARE
 	void selectChapter();
 #endif
 	void selectAutoLang();
