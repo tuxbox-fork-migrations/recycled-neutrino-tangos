4a823835b95838e871fe747c2503fbac7b3883aa
TangoCash <eric@loxat.de>
2020-01-09 09:02:36 +0100

    Revert "fix path"
    This reverts commit a05ec3e07413d0e300e1b1e8418264b2e5a84024.
    
    
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/7647d6040426da7c791225b069c672a2e688e7ac
    Author: TangoCash <eric@loxat.de>
    Date: 2020-01-09 (Thu, 09 Jan 2020)
    
    
    
    ------------------
    This commit was generated by Migit

--- src/gui/update.cpp
+++ src/gui/update.cpp
@@ -204,7 +204,7 @@ bool CFlashUpdate::checkOnlineVersion()
 
 	CConfigFile _configfile('\t');
 	std::string versionString = "????????????????";
-	if (_configfile.loadConfig("/.version"))
+	if (_configfile.loadConfig(TARGET_PREFIX "/.version"))
 		versionString = _configfile.getString("version", "????????????????");
 	dprintf(DEBUG_NORMAL, "[update] file %s\n", g_settings.softupdate_url_file.c_str());
 	CFlashVersionInfo curInfo(versionString.c_str());
