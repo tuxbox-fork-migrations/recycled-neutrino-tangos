f58df2a6a786a90bd2cf145c3c1dd115695bc03e
vanhofen <vanhofen@gmx.de>
2020-11-12 22:17:34 +0100

    helpers: add function to get backup suffix
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/ecfa70b7014d76b9195ceb3b48d3c2dcf7b15737
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2020-11-12 (Thu, 12 Nov 2020)
    
    Origin message was:
    ------------------
    - helpers: add function to get backup suffix
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/system/helpers.cpp
+++ src/system/helpers.cpp
@@ -54,6 +54,7 @@
 //#include <driver/framebuffer.h>
 #include <system/helpers.h>
 #include <system/helpers-json.h>
+#include <lib/libnet/libnet.h>
 #include <gui/update_ext.h>
 #include <driver/framebuffer.h>
 #include <libmd5sum.h>
@@ -500,6 +501,13 @@ std::string getFileExt(std::string &file)
 	return _getBaseName(f, ".");
 }
 
+std::string getBackupSuffix()
+{
+	std::string hostName = "";
+	netGetHostname(hostName);
+
+	return hostName + getNowTimeStr("_%Y%m%d_%H%M");
+}
 
 std::string getNowTimeStr(const char* format)
 {
