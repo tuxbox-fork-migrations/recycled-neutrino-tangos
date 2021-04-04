ecb93b2abc405ea8b2cb6d197474d14f3276c0fe
TangoCash <eric@loxat.de>
2021-04-04 10:02:00 +0200

    bump lua api to 1.89
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/43124619a74d825e61b3ae2c68705ea8c3bb249a
    Author: TangoCash <eric@loxat.de>
    Date: 2021-04-04 (Sun, 04 Apr 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/lua/lua_cc_window.cpp
+++ src/gui/lua/lua_cc_window.cpp
@@ -57,6 +57,7 @@ void CLuaInstCCWindow::CCWindowRegister(lua_State *L)
 		{ "footer_height",  CLuaInstCCWindow::CCWindowGetFooterHeight_dep }, /* function 'footer_height' is deprecated */
 		{ "setCenterPos",   CLuaInstCCWindow::CCWindowSetCenterPos },
 		{ "setDimensionsAll", CLuaInstCCWindow::CCWindowSetDimensionsAll },
+		{ "setBodyImage",   CCWindowSetBodyImage },
 		{ "__gc",           CLuaInstCCWindow::CCWindowDelete },
 		{ NULL, NULL }
 	};
@@ -94,6 +95,7 @@ int CLuaInstCCWindow::CCWindowNew(lua_State *L)
 	std::string btnPlay       = "";
 	std::string btnPlayPause  = "";
 	std::string btnOk         = "";
+	std::string btnSetup      = "";
 	lua_Integer x = 100, y = 100, dx = 450, dy = 250;
 	tableLookup(L, "x", x);
 	tableLookup(L, "y", y);
@@ -131,6 +133,7 @@ int CLuaInstCCWindow::CCWindowNew(lua_State *L)
 	tableLookup(L, "btnPlay", btnPlay);
 	tableLookup(L, "btnPlayPause", btnPlayPause);
 	tableLookup(L, "btnOk", btnOk);
+	tableLookup(L, "btnSetup", btnSetup);
 	color_frame  = checkMagicMask(color_frame);
 	color_body   = checkMagicMask(color_body);
 	color_shadow = checkMagicMask(color_shadow);
@@ -267,6 +270,12 @@ int CLuaInstCCWindow::CCWindowNew(lua_State *L)
 				btnSOk.button	= NEUTRINO_ICON_BUTTON_OKAY;
 				btnSOk.text	= btnOk;
 				buttons.push_back(btnSOk);
+			}
+			if (!btnSetup.empty()) {
+				button_label_cc btnSSetup;
+				btnSSetup.button	= NEUTRINO_ICON_BUTTON_MENU;
+				btnSSetup.text		= btnSetup;
+				buttons.push_back(btnSSetup);
 			}
 			if (!buttons.empty())
 				footer->setButtonLabels(buttons, footer->getWidth(), footer->getWidth() / buttons.size());
@@ -441,6 +450,20 @@ int CLuaInstCCWindow::CCWindowSetCenterPos(lua_State *L)
 		along_mode=tmp_along_mode;
 
 	D->w->setCenterPos(along_mode);
+	return 0;
+}
+
+int CLuaInstCCWindow::CCWindowSetBodyImage(lua_State *L)
+{
+	lua_assert(lua_istable(L,1));
+	CLuaCCWindow *D = CCWindowCheck(L, 1);
+	if (!D) return 0;
+
+	std::string image = "";
+	if (lua_istable(L, -1))
+		tableLookup(L, "image_path", image);
+
+	D->w->setBodyBGImage(image);
 	return 0;
 }
 
