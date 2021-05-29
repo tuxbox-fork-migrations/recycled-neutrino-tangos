8c7c329ba4bede1773742063d665f2b5f8b71d0e
vanhofen <vanhofen@gmx.de>
2021-05-29 21:16:57 +0200

    addition to use autodetect pip
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/24920a7677fb3650493c5218737f2897fd455968
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2021-05-29 (Sat, 29 May 2021)
    
    Origin message was:
    ------------------
    - addition to use autodetect pip
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/zapit/src/zapit.cpp
+++ src/zapit/src/zapit.cpp
@@ -670,7 +670,8 @@ bool CZapit::ZapIt(const t_channel_id channel_id, bool forupdate, bool startplay
 #ifdef ENABLE_PIP
 bool CZapit::StopPip()
 {
-	if (!g_info.hw_caps->can_pip) return false;
+	if (!g_info.hw_caps->can_pip)
+		return false;
 
 	if (CNeutrinoApp::getInstance()->avinput_pip) {
 		CNeutrinoApp::getInstance()->StopAVInputPiP();
@@ -691,7 +692,8 @@ bool CZapit::StopPip()
 
 bool CZapit::StartPip(const t_channel_id channel_id)
 {
-	if (!g_info.hw_caps->can_pip) return false;
+	if (!g_info.hw_caps->can_pip)
+		return false;
 
 	CZapitChannel* newchannel;
 	bool transponder_change;
