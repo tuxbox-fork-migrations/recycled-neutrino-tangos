ab832cc99ead607c6d938129bf4895e9e94680cc
TangoCash <eric@loxat.de>
2021-09-02 17:10:56 +0200

    add frequently reload xmltv epg
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/ed6157732c31a1ad9a70f1106141849a2aa5d629
    Author: TangoCash <eric@loxat.de>
    Date: 2021-09-02 (Thu, 02 Sep 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/eitd/sectionsd.cpp
+++ src/eitd/sectionsd.cpp
@@ -2188,6 +2188,8 @@ static void *houseKeepingThread(void *)
 					perror("sectionsd: pthread_create()");
 					}
 				pthread_attr_destroy(&attr);
+
+				eventServer->sendEvent(CSectionsdClient::EVT_RELOAD_XMLTV, CEventServer::INITID_SECTIONSD);
 			}
 			ecount = 0;
 		}
