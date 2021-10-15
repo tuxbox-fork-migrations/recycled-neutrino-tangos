e718c473b2a69336e9a48089535a107cc8e5cdd5
TangoCash <eric@loxat.de>
2021-10-15 08:19:56 +0200

    cleanup
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/d3a5788449622e4ae793d5a060470fb6cffab9ae
    Author: TangoCash <eric@loxat.de>
    Date: 2021-10-15 (Fri, 15 Oct 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/zapit/src/zapit.cpp
+++ src/zapit/src/zapit.cpp
@@ -2312,15 +2312,6 @@ bool CZapit::StartPlayBack(CZapitChannel *thisChannel)
 		pcrDemux->Start();
 	}
 
-#if HAVE_AZBOX_HARDWARE
-	/* new (> 20130917) AZbox drivers switch to radio mode if audio is started first */
-	/* start video */
-	if (video_pid) {
-		videoDecoder->Start(0, thisChannel->getPcrPid(), thisChannel->getVideoPid());
-		videoDemux->Start();
-	}
-#endif
-
 	/* select audio output and start audio */
 	if (audio_pid) {
 		SetAudioStreamType(thisChannel->getAudioChannel()->audioChannelType);
@@ -2328,7 +2319,6 @@ bool CZapit::StartPlayBack(CZapitChannel *thisChannel)
 		audioDecoder->Start();
 	}
 
-#if ! HAVE_AZBOX_HARDWARE
 	/* start video */
 	if (video_pid) {
 	#if HAVE_COOL_HARDWARE
@@ -2339,7 +2329,7 @@ bool CZapit::StartPlayBack(CZapitChannel *thisChannel)
 		videoDecoder->Start(0, pcr_pid, video_pid);
 	#endif
 	}
-#endif
+
 #ifdef USE_VBI
 	if(teletext_pid)
 		videoDecoder->StartVBI(teletext_pid);
@@ -2368,16 +2358,6 @@ bool CZapit::StopPlayBack(bool send_pmt, bool __attribute__ ((unused)) blank)
 	if (playbackStopForced)
 		return false;
 
-#if HAVE_AZBOX_HARDWARE
-	pcrDemux->Stop();
-
-	if (current_channel && current_channel->getVideoPid()) {
-		videoDemux->Stop();
-		videoDecoder->Stop(standby ? false : true);
-	}
-	audioDemux->Stop();
-	audioDecoder->Stop();
-#else
 	videoDecoder->Stop(false);
 	videoDemux->Stop();
 	audioDemux->Stop();
