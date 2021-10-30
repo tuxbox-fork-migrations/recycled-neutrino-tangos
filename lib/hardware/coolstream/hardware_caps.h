122615f309dfc96a35b2db103c3d308445098f40
TangoCash <eric@loxat.de>
2021-10-30 10:12:37 +0200

    align /lib
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/6b14821368ec870710eef75b951c0d124808184a
    Author: TangoCash <eric@loxat.de>
    Date: 2021-10-30 (Sat, 30 Oct 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- lib/hardware/coolstream/hardware_caps.h
+++ lib/hardware/coolstream/hardware_caps.h
@@ -23,9 +23,13 @@ typedef struct hw_caps
 {
 	int has_fan;
 	int has_HDMI;
+	int has_HDMI_input;
 	int has_SCART;
 	int has_SCART_input;
 	int has_YUV_cinch;
+	int can_pip;
+	int pip_devs;
+	int can_cpufreq;
 	int can_shutdown;
 	int can_cec;
 	int can_ar_14_9;	/* video drivers have 14:9 aspect ratio mode */
@@ -34,15 +38,21 @@ typedef struct hw_caps
 	display_type_t display_type;
 	int display_xres;	/* x resolution or chars per line */
 	int display_yres;
-	int display_can_deepstandby;
 	int display_can_set_brightness;
+	int display_can_deepstandby;
+	int display_can_umlauts;
 	int display_has_statusline;
+	int display_has_colon;
 	int has_button_timer;
 	int has_button_vformat;
+	char startup_file[64];
 	char boxvendor[64];
 	char boxname[64];
 	char boxarch[64];
+	int boxtype;
+	int has_CI;
 } hw_caps_t;
 
 hw_caps_t *get_hwcaps(void);
-#endif
+
+#endif // __HARDWARE_CAPS_H__
