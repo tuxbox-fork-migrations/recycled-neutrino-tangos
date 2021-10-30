5e63c624c3812fddf1f721dbc99f668933afc32e
TangoCash <eric@loxat.de>
2021-10-30 12:08:33 +0200

    cleanup-5
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/d0aa9d3cef2966e14e4c465bdb2f4802ca06171e
    Author: TangoCash <eric@loxat.de>
    Date: 2021-10-30 (Sat, 30 Oct 2021)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/rcinput.h
+++ src/driver/rcinput.h
@@ -303,7 +303,6 @@ class CRCInput
 			RC_analog_on	= KEY_ANALOGON,
 			RC_analog_off	= KEY_ANALOGOFF,
 
-			/* tripledragon keys */
 			RC_eject	= KEY_EJECTCD,
 			RC_aux		= KEY_AUX,          /* 0x186 */
 			RC_timer	= KEY_TIME,
@@ -321,9 +320,6 @@ class CRCInput
 		enum
 		{
 			RC_HW_COOLSTREAM	= 0,
-			RC_HW_DBOX		= 1,
-			RC_HW_PHILIPS		= 2,
-			RC_HW_TRIPLEDRAGON	= 3
 		};
 		void set_rc_hw(void);
 
