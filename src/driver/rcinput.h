7eadf8c5b0d5bfea8ff38cbe770b3efa6b2e7ddc
vanhofen <vanhofen@gmx.de>
2022-07-15 22:07:54 +0200

    rcinput: add RC_f1..10 keys
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/fd96704e8e159781152d87bafa06fc6c10b697db
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2022-07-15 (Fri, 15 Jul 2022)
    
    Origin message was:
    ------------------
    - rcinput: add RC_f1..10 keys
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/rcinput.h
+++ src/driver/rcinput.h
@@ -312,6 +312,17 @@ class CRCInput
 			RC_zoomin	= KEY_ZOOMIN,
 			RC_zoomout	= KEY_ZOOMOUT,
 
+			RC_f1		= KEY_F1,
+			RC_f2		= KEY_F2,
+			RC_f3		= KEY_F3,
+			RC_f4		= KEY_F4,
+			RC_f5		= KEY_F5,
+			RC_f6		= KEY_F6,
+			RC_f7		= KEY_F7,
+			RC_f8		= KEY_F8,
+			RC_f9		= KEY_F9,
+			RC_f10		= KEY_F10,
+
 			RC_timeout	= 0xFFFFFFFF,
 			RC_nokey	= RC_NOKEY
 		};
