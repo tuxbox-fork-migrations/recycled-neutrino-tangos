4d419c1def80472dcbff5bff436df4683f18974a
TangoCash <eric@loxat.de>
2022-09-06 14:04:37 +0200

    rework streaming, add e2 streaming api
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/f680736e1dbf86bad23e4613c2a70f102de8d15b
    Author: TangoCash <eric@loxat.de>
    Date: 2022-09-06 (Tue, 06 Sep 2022)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/zapit/src/channel.cpp
+++ src/zapit/src/channel.cpp
@@ -123,6 +123,7 @@ CZapitChannel::~CZapitChannel(void)
 	//setCaPmt(NULL);
 	setRawPmt(NULL);
 	camap.clear();
+	capids.clear();
 }
 
 CZapitAudioChannel *CZapitChannel::getAudioChannel(unsigned char index)
