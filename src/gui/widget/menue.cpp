1f091548b810e38ab4f3f1f32476840bb93d2701
vanhofen <vanhofen@gmx.de>
2022-09-14 20:47:52 +0200

    menue: change case of new separatorline switch
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/32aa4b8123bea7409474ae0a9da01987058d67df
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2022-09-14 (Wed, 14 Sep 2022)
    
    Origin message was:
    ------------------
    - menue: change case of new separatorline switch
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -1610,7 +1610,7 @@ void CMenuWidget::paintItems()
 }
 
 /*adds the typical menu intro with optional subhead, separator, back or cancel button and separatorline to menu*/
-void CMenuWidget::addIntroItems(const std::string &s_subhead_text, neutrino_locale_t section_text, int buttontype, bool brief_hint, bool SeparatorLine)
+void CMenuWidget::addIntroItems(const std::string &s_subhead_text, neutrino_locale_t section_text, int buttontype, bool brief_hint, bool separatorline)
 {
 	brief_hints = brief_hint;
 
@@ -1631,7 +1631,7 @@ void CMenuWidget::addIntroItems(const std::string &s_subhead_text, neutrino_loca
 			break;
 	}
 
-	if (SeparatorLine) {
+	if (separatorline) {
 		if (section_text != NONEXISTANT_LOCALE)
 			addItem(new CMenuSeparator(CMenuSeparator::LINE | CMenuSeparator::STRING, section_text));
 		else if (buttontype != BTN_TYPE_NO)
@@ -1639,10 +1639,10 @@ void CMenuWidget::addIntroItems(const std::string &s_subhead_text, neutrino_loca
 	}
 }
 
-void CMenuWidget::addIntroItems(neutrino_locale_t l_subhead_text, neutrino_locale_t section_text, int buttontype, bool brief_hint, bool SeparatorLine)
+void CMenuWidget::addIntroItems(neutrino_locale_t l_subhead_text, neutrino_locale_t section_text, int buttontype, bool brief_hint, bool separatorline)
 {
 	std::string str = l_subhead_text == NONEXISTANT_LOCALE ? "" : g_Locale->getText(l_subhead_text);
-	addIntroItems(str, section_text, buttontype, brief_hint, SeparatorLine);
+	addIntroItems(str, section_text, buttontype, brief_hint, separatorline);
 }
 
 void CMenuWidget::saveScreen()
