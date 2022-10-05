0d8f62a0efaf10021d35e47dac86dfee17e00206
TangoCash <eric@loxat.de>
2022-10-05 17:24:27 +0200

    rework streaming part1
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/5194c462e29c4fb9531c91755e359c48e17b4ae1
    Author: TangoCash <eric@loxat.de>
    Date: 2022-10-05 (Wed, 05 Oct 2022)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/streamts.h
+++ src/driver/streamts.h
@@ -53,14 +53,13 @@ class CStreamInstance : public OpenThreads::Thread
 		t_channel_id channel_id;
 		stream_pids_t pids;
 		stream_fds_t fds;
-		bool send_raw;
 
 		virtual bool Send(ssize_t r, unsigned char * _buf = NULL);
 		virtual void Close();
 		virtual void run();
 		friend class CStreamManager;
 	public:
-		CStreamInstance(int clientfd, t_channel_id chid, stream_pids_t &pids, bool send_raw);
+		CStreamInstance(int clientfd, t_channel_id chid, stream_pids_t &pids);
 		virtual ~CStreamInstance();
 		virtual bool Open();
 		virtual bool Start();
@@ -70,6 +69,7 @@ class CStreamInstance : public OpenThreads::Thread
 		bool HasFd(int fd);
 		stream_fds_t & GetFds() { return fds; }
 		t_channel_id GetChannelId() { return channel_id; }
+		bool is_e2_stream;
 };
 
 class CStreamStream : public CStreamInstance
@@ -120,8 +120,8 @@ class CStreamManager : public OpenThreads::Thread
 		streammap_t streams;
 
 		bool	Listen();
-		bool	Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFrontend * &frontend, bool &send_raw);
-		void	AddPids(int fd, CZapitChannel * channel, stream_pids_t &pids, bool send_raw);
+		bool	Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFrontend * &frontend, bool &is_e2);
+		void	PreparePids(CZapitChannel * channel, stream_pids_t &pids);
 		void	CheckStandby(bool enter);
 		CFrontend * FindFrontend(CZapitChannel * channel);
 		bool	StopAll();
