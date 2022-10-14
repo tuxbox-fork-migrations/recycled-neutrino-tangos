a13fd3e12bd256cfc658f1ae73c86ed8a616c48f
TangoCash <eric@loxat.de>
2022-10-14 16:29:40 +0200

    remove lib/hardware
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/5b529bfe65873ba5387aaeaf83d831e14f76cb96
    Author: TangoCash <eric@loxat.de>
    Date: 2022-10-14 (Fri, 14 Oct 2022)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- lib/hardware/audio.h
+++
@@ -1,8 +0,0 @@
-#if HAVE_CST_HARDWARE
-#include <audio_cs.h>
-#elif USE_STB_HAL
-#include <audio_hal.h>
-#else
-#error neither HAVE_CST_HARDWARE nor USE_STB_HAL defined.
-#error do you need to include config.h?
-#endif
--- lib/hardware/ca.h
+++
@@ -1,8 +0,0 @@
-#if HAVE_CST_HARDWARE
-#include <ca_cs.h>
-#elif USE_STB_HAL
-#include <ca_hal.h>
-#else
-#error neither HAVE_CST_HARDWARE nor USE_STB_HAL defined.
-#error do you need to include config.h?
-#endif
