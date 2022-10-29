15220a3ab7236340dfb2eb2fdaab124095e41a3f
Frankenstone <dampf_acc@online.de>
2022-10-29 20:55:23 +0200

    rcinput: add RC_pvr
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/4f6b1e154c2622d3164539fe75e4c8e822fca242
    Author: Frankenstone <dampf_acc@online.de>
    Date: 2022-10-29 (Sat, 29 Oct 2022)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/rcinput.cpp
+++ src/driver/rcinput.cpp
@@ -1768,6 +1768,8 @@ const char * CRCInput::getSpecialKeyName(const unsigned int key)
 				return "program";
 			case RC_playpause:
 				return "play / pause";
+			case RC_pvr:
+				return "pvr";
 #if BOXMODEL_HD51 || BOXMODEL_MULTIBOX || BOXMODEL_MULTIBOXSE || BOXMODEL_HD60 || BOXMODEL_HD61 || BOXMODEL_BRE2ZE4K || BOXMODEL_H7 || BOXMODEL_OSMIO4K || BOXMODEL_OSMIO4KPLUS
 			case RC_playpause_long:
 				return "play / pause long";
