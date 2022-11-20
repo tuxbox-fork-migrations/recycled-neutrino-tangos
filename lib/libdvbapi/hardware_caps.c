65128296affd50bbb35df4e8035046a0572c593b
TangoCash <eric@loxat.de>
2022-11-20 17:48:27 +0100

    fix protek4k
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/5f560629b99dfb7bc611a9693f31a3d7f37a7d1a
    Author: TangoCash <eric@loxat.de>
    Date: 2022-11-20 (Sun, 20 Nov 2022)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- lib/libdvbapi/hardware_caps.c
+++ lib/libdvbapi/hardware_caps.c
@@ -496,5 +496,23 @@ hw_caps_t *get_hwcaps(void)
 	strcpy(caps.boxname, "E4HD 4K ULTRA");
 	strcpy(caps.boxarch, "BCM7252S");
 #endif
+#if BOXMODEL_PROTEK4K
+	initialized = 1;
+	caps.has_CI = 1;
+	caps.can_cec = 1;
+	caps.can_shutdown = 1;
+	caps.display_xres = 220;
+	caps.display_yres = 176;
+	caps.display_type = HW_DISPLAY_GFX;
+	caps.display_can_deepstandby = 0;
+	caps.display_can_set_brightness = 1;
+	caps.display_has_statusline = 0;
+	caps.display_has_colon = 0;
+	caps.has_button_timer = 1;
+	caps.has_HDMI = 1;
+	strcpy(caps.boxvendor, "PROTEK");
+	strcpy(caps.boxname, "PROTEK 4K");
+	strcpy(caps.boxarch, "BCM7252S");
+#endif
 	return &caps;
 }
