28b477b74760d8818763fb6bf7b6e92fc7757102
TangoCash <eric@loxat.de>
2024-05-10 16:24:25 +0200

    switch to simple mpv playback for generic
    Origin commit data
    ------------------
    Commit: https://github.com/TangoCash/neutrino-tangos/commit/ee7563583526e3c04216b9e0d2b35c1027d880a9
    Author: TangoCash <eric@loxat.de>
    Date: 2024-05-10 (Fri, 10 May 2024)
    
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- lib/libdvbapi/playback_pc.h
+++ lib/libdvbapi/playback_pc.h
@@ -1,40 +1,9 @@
-/*
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
-#ifndef __PLAYBACK_GST_H__
-#define __PLAYBACK_GST_H__
+#ifndef __PLAYBACK_LIB_H__
+#define __PLAYBACK_LIB_H__
 
 #include <string>
 #include <stdint.h>
 #include <vector>
-
-#include <config.h>
-
-
-typedef enum
-{
-	STATE_STOP,
-	STATE_PLAY,
-	STATE_PAUSE,
-	STATE_FF,
-	STATE_REW,
-	STATE_SLOW
-} playstate_t;
 
 typedef enum
 {
@@ -43,60 +12,50 @@ typedef enum
 } playmode_t;
 
 struct AVFormatContext;
-
 class cPlayback
 {
 	private:
-		bool playing, first;
-		bool decoders_closed;
-
-		int mSpeed;
+		bool playing;
 		int mAudioStream;
-		int init_jump;
+		int mSubtitleStream;
+		int mTeletextStream;
 
 	public:
-		playstate_t playstate;
-
 		cPlayback(int);
 		bool Open(playmode_t PlayMode);
 		void Close(void);
 		bool Start(char *filename, int vpid, int vtype, int apid, int ac3, int duration, std::string headers = "");
 		bool Start(std::string filename, std::string headers = "");
-		bool Play(void);
-		bool SyncAV(void);
-
-		bool Stop(void);
 		bool SetAPid(int pid, bool ac3);
 		bool SetSubtitlePid(int pid);
 		bool SetTeletextPid(int pid);
-
-		void trickSeek(int ratio);
+		int GetAPid(void) { return mAudioStream; }
+		int GetVPid(void);
+		int GetSubtitlePid(void) { return mSubtitleStream; }
+		int GetTeletextPid(void);
+		void SuspendSubtitle(bool);
+		int GetFirstTeletextPid(void);
 		bool SetSpeed(int speed);
-		bool SetSlow(int slow);
 		bool GetSpeed(int &speed) const;
 		bool GetPosition(int &position, int &duration, bool isWebChannel = false);
 		void GetPts(uint64_t &pts);
-		int GetAPid(void);
-		int GetVPid(void);
-		int GetSubtitlePid(void);
 		bool SetPosition(int position, bool absolute = false);
 		void FindAllPids(int *apids, unsigned int *ac3flags, unsigned int *numpida, std::string *language);
 		void FindAllSubtitlePids(int *pids, unsigned int *numpids, std::string *language);
 		void FindAllTeletextsubtitlePids(int *pids, unsigned int *numpidt, std::string *tlanguage, int *mags, int *pages);
 		void RequestAbort(void);
+		bool IsPlaying(void) { return false; }
+		uint64_t GetReadCount(void);
 		void FindAllSubs(uint16_t *pids, unsigned short *supported, uint16_t *numpida, std::string *language);
 		bool SelectSubtitles(int pid);
-		uint64_t GetReadCount(void);
+		void GetTitles(std::vector<int> &playlists, std::vector<std::string> &titles, int &current);
+		void SetTitle(int title);
 		void GetChapters(std::vector<int> &positions, std::vector<std::string> &titles);
 		void GetMetadata(std::vector<std::string> &keys, std::vector<std::string> &values);
-		AVFormatContext *GetAVFormatContext();
-		void ReleaseAVFormatContext();
-		std::string extra_headers;
-		std::string user_agent;
-
 		//
 		~cPlayback();
-		void getMeta();
+		AVFormatContext *GetAVFormatContext() { return NULL; }
+		void ReleaseAVFormatContext() {}
 };
 
-#endif // __PLAYBACK_GST_H__
+#endif // __PLAYBACK_LIB_H__
